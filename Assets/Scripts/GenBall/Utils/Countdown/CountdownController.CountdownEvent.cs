e1fb12047c2c45e72f67eda7a30a2163a5db0543
Yueyn <1689759238@qq.com>
2025-11-16 17:23:17 +0800

    倒计时管理器

---
+++ Assets/Scripts/GenBall/Utils/Countdown/CountdownController.CountdownEvent.cs
@@ -0,0 +1,84 @@
+using System;
+using Yueyn.Base.ReferencePool;
+using Yueyn.Base.Variable;
+
+namespace GenBall.Utils.Countdown
+{
+    public partial class CountdownController
+    {
+        private class CountdownEvent : IReference
+        {
+            private float _countdownTime;
+            /// <summary>
+            /// 传入参数为剩余冷却时间
+            /// </summary>
+            private Action<float> _updateCallback;
+            private Action _completeCallback;
+
+            private float _timer;
+            
+            private bool _hasTrigger;
+            private bool _paused;
+            
+            public bool Compeleted => _timer>=_countdownTime;
+            
+            public void Pause()
+            {
+                _paused = true;
+            }
+
+            public void Resume()
+            {
+                _paused = false;
+            }
+
+            public void Start()
+            {
+                _timer = 0;
+                _hasTrigger=false;
+            }
+            public void Update(float deltaTime)
+            {
+                if(_paused) return;
+                if(_hasTrigger) return;
+                if (_timer >= _countdownTime)
+                {
+                    _hasTrigger = true;
+                    _timer = 0;
+                    _completeCallback?.Invoke();
+                }
+                _timer += deltaTime;
+                _updateCallback?.Invoke(_countdownTime-_timer);
+            }
+            
+            /// <summary>
+            /// 创建时是已经完成计时的状态，需要手动开始计时
+            /// </summary>
+            /// <param name="countdownTime"></param>
+            /// <param name="updateCallback"></param>
+            /// <param name="completeCallback"></param>
+            /// <returns></returns>
+            public static CountdownEvent Create(float countdownTime, Action<float> updateCallback=null, Action completeCallback=null)
+            {
+                var countdownEvent = ReferencePool.Acquire<CountdownEvent>();
+                countdownEvent._countdownTime = countdownTime;
+                countdownEvent._updateCallback = updateCallback;
+                countdownEvent._completeCallback = completeCallback;
+                countdownEvent._timer = countdownTime+0.01f;
+                countdownEvent._hasTrigger = true;
+                countdownEvent._paused = false;
+                return countdownEvent;
+            }
+
+            public void Clear()
+            {
+                _countdownTime = 0;
+                _timer = 0;
+                _updateCallback = null;
+                _completeCallback = null;
+                _hasTrigger = false;
+                _paused = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GenBall/Utils/Countdown/CountdownController.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Yueyn.Base.ReferencePool;
+using Yueyn.Base.Variable;
+
+namespace GenBall.Utils.Countdown
+{
+    public partial class CountdownController
+    {
+        private readonly Dictionary<string, CountdownEvent> _countdownEvents = new();
+
+        public bool HasCountdownEvent(string name)=> _countdownEvents.ContainsKey(name);
+
+        public bool HasCountdownCompleted(string name)
+        {
+            if (_countdownEvents.TryGetValue(name, out var countdownEvent))
+            {
+                return countdownEvent.Compeleted;
+            }
+            throw new Exception("Countdown event not found");
+        }
+        
+        public void Update(float deltaTime)
+        {
+            foreach (var countdownEvent in _countdownEvents.Values)
+            {
+                countdownEvent.Update(deltaTime);
+            }
+        }
+
+        public void AddCountdownEvent(string name,float countdownTime,Action<float> updateCallback=null,Action completeCallback=null)
+        {
+            if (_countdownEvents.ContainsKey(name))
+            {
+                throw new Exception("Countdown Event has already been registered");
+            }
+            var countdown=CountdownEvent.Create(countdownTime, updateCallback, completeCallback);
+        }
+
+        public bool RemoveCountdownEvent(string name)
+        {
+            if (_countdownEvents.Remove(name,out CountdownEvent countdown))
+            {
+                ReferencePool.Release(countdown);
+            }
+            return false;
+        }
+
+        public void RemoveAllCountdownEvents()
+        {
+            var countdownEvents=new List<CountdownEvent>();
+            countdownEvents.AddRange(_countdownEvents.Values);
+            _countdownEvents.Clear();
+            foreach (var countdownEvent in countdownEvents)
+            {
+                ReferencePool.Release(countdownEvent);
+            }
+            countdownEvents.Clear();
+        }
+    }
+}
