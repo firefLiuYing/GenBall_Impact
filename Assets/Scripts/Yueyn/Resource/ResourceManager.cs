3db38a96a4bbf55f39464c27799f50acbe5afb05
Yueyn <1689759238@qq.com>
2025-11-17 19:20:11 +0800

    预制菜资源加载修改

--- Assets/Scripts/Yueyn/Resource/ResourceManager.cs
+++ Assets/Scripts/Yueyn/Resource/ResourceManager.cs
@@ -1,15 +1,38 @@
 ﻿using System;
 using UnityEditor;
 using UnityEngine;
+using Yueyn.Main;
 
 namespace Yueyn.Resource
 {
-    public class ResourceManager
+    public class ResourceManager:IComponent
     {
         public void LoadPrefab(string path, Action<object> callback)
         {
             GameObject prefab=AssetDatabase.LoadAssetAtPath<GameObject>(path);
             callback?.Invoke(prefab);
+        }
+
+        public object LoadPrefab(string path)=>AssetDatabase.LoadAssetAtPath<GameObject>(path);
+
+        public void OnRegister()
+        {
+            
+        }
+
+        public void OnUnregister()
+        {
+            
+        }
+
+        public void Update(float elapsedSeconds, float realElapseSeconds)
+        {
+            
+        }
+
+        public void Shutdown()
+        {
+            
         }
     }
 }
