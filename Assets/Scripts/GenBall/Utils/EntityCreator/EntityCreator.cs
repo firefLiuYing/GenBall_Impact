8a1221ad875366f89492eea9c4b655bf426604bc
Yueyn <1689759238@qq.com>
2025-11-18 20:55:14 +0800

    实体池拓展指定位置生成实体

--- Assets/Scripts/GenBall/Utils/EntityCreator/EntityCreator.cs
+++ Assets/Scripts/GenBall/Utils/EntityCreator/EntityCreator.cs
@@ -24,9 +24,25 @@ namespace GenBall.Utils.EntityCreator
             _entityPool.Despawn(entity);
             _prefabs.Remove(entity.GetComponent<TEntityInterface>());
         }
+
+        #region CreateEntity
+
         public TEntity CreateEntity<TEntity>() where TEntity : TEntityInterface => (TEntity)CreateEntity(new TypeNamePair(typeof(TEntity)));
         public TEntity CreateEntity<TEntity>(string name)where TEntity:TEntityInterface =>(TEntity)CreateEntity(new TypeNamePair(typeof(TEntity), name));
         
+        public TEntity CreateEntity<TEntity>(Transform parent) where TEntity : TEntityInterface 
+            =>(TEntity)CreateEntity(new TypeNamePair(typeof(TEntity)), parent);
+        public TEntity CreateEntity<TEntity>(string name,Transform parent) where TEntity : TEntityInterface 
+            =>(TEntity)CreateEntity(new TypeNamePair(typeof(TEntity), name), parent);
+        
+        public TEntity CreateEntity<TEntity>(Vector3 position,Quaternion rotation) where TEntity : TEntityInterface 
+            =>(TEntity)CreateEntity(new TypeNamePair(typeof(TEntity)), position, rotation);
+        public TEntity CreateEntity<TEntity>(string name,Vector3 position,Quaternion rotation) where TEntity : TEntityInterface 
+            =>(TEntity)CreateEntity(new TypeNamePair(typeof(TEntity), name), position, rotation);
+        public TEntity CreateEntity<TEntity>(Vector3 position,Quaternion rotation,Transform parent) where TEntity : TEntityInterface 
+            =>(TEntity)CreateEntity(new TypeNamePair(typeof(TEntity)), position, rotation, parent);
+        public TEntity CreateEntity<TEntity>(string name,Vector3 position,Quaternion rotation,Transform parent) where TEntity : TEntityInterface 
+            =>(TEntity)CreateEntity(new TypeNamePair(typeof(TEntity), name), position, rotation, parent);
         private TEntityInterface CreateEntity(TypeNamePair typeNamePair)
         {
             var entityObject = _entityPool.Spawn($"{typeNamePair}");
@@ -51,6 +67,94 @@ namespace GenBall.Utils.EntityCreator
             _entityPool.Register(entityObject,true);
             return entityInterface;
         }
+        
+        private TEntityInterface CreateEntity(TypeNamePair typeNamePair, Transform parent)
+        {
+            var entityObject = _entityPool.Spawn($"{typeNamePair}");
+            TEntityInterface entityInterface;
+            GameObject go;
+            if (entityObject != null)
+            {
+                go = (GameObject)entityObject.Target;
+                go.transform.SetParent(parent,false);
+                // go.transform.SetPositionAndRotation(position, rotation);
+                entityInterface = go.GetComponent<TEntityInterface>();
+                _prefabs.Add(entityInterface);
+                return entityInterface;
+            }
+
+            if (!_prefabMap.TryGetValue(typeNamePair, out var prefabPath))
+            {
+                throw new Exception($"Entity Prefab {typeNamePair} is not registered");
+            }
+            var prefab=(GameObject)ResourceManager.LoadPrefab(prefabPath);
+            go = Object.Instantiate(prefab, parent,false);
+            entityInterface = go.GetComponent<TEntityInterface>();
+            if (entityInterface == null) throw new Exception($"Entity Prefab not found Component {typeof(TEntityInterface).FullName}");
+            _prefabs.Add(entityInterface);
+            entityObject = EntityObject.Create($"{typeNamePair}", go);
+            _entityPool.Register(entityObject,true);
+            return entityInterface;
+        }
+        
+        private TEntityInterface CreateEntity(TypeNamePair typeNamePair, Vector3 position, Quaternion rotation)
+        {
+            var entityObject = _entityPool.Spawn($"{typeNamePair}");
+            TEntityInterface entityInterface;
+            GameObject go;
+            if (entityObject != null)
+            {
+                go = (GameObject)entityObject.Target;
+                // go.transform.SetParent(parent);
+                go.transform.SetPositionAndRotation(position, rotation);
+                entityInterface = go.GetComponent<TEntityInterface>();
+                _prefabs.Add(entityInterface);
+                return entityInterface;
+            }
+
+            if (!_prefabMap.TryGetValue(typeNamePair, out var prefabPath))
+            {
+                throw new Exception($"Entity Prefab {typeNamePair} is not registered");
+            }
+            var prefab=(GameObject)ResourceManager.LoadPrefab(prefabPath);
+            go = Object.Instantiate(prefab, position, rotation);
+            entityInterface = go.GetComponent<TEntityInterface>();
+            if (entityInterface == null) throw new Exception($"Entity Prefab not found Component {typeof(TEntityInterface).FullName}");
+            _prefabs.Add(entityInterface);
+            entityObject = EntityObject.Create($"{typeNamePair}", go);
+            _entityPool.Register(entityObject,true);
+            return entityInterface;
+        }
+        private TEntityInterface CreateEntity(TypeNamePair typeNamePair, Vector3 position, Quaternion rotation,Transform parent)
+        {
+            var entityObject = _entityPool.Spawn($"{typeNamePair}");
+            TEntityInterface entityInterface;
+            GameObject go;
+            if (entityObject != null)
+            {
+                go = (GameObject)entityObject.Target;
+                go.transform.SetParent(parent);
+                go.transform.SetPositionAndRotation(position, rotation);
+                entityInterface = go.GetComponent<TEntityInterface>();
+                _prefabs.Add(entityInterface);
+                return entityInterface;
+            }
+
+            if (!_prefabMap.TryGetValue(typeNamePair, out var prefabPath))
+            {
+                throw new Exception($"Entity Prefab {typeNamePair} is not registered");
+            }
+            var prefab=(GameObject)ResourceManager.LoadPrefab(prefabPath);
+            go = Object.Instantiate(prefab, position, rotation, parent);
+            entityInterface = go.GetComponent<TEntityInterface>();
+            if (entityInterface == null) throw new Exception($"Entity Prefab not found Component {typeof(TEntityInterface).FullName}");
+            _prefabs.Add(entityInterface);
+            entityObject = EntityObject.Create($"{typeNamePair}", go);
+            _entityPool.Register(entityObject,true);
+            return entityInterface;
+        }
+
+        #endregion
         public void AddPrefab<TEntity>(string prefabPath) where TEntity : class, TEntityInterface => AddPrefab(new TypeNamePair(typeof(TEntity)),prefabPath);
         public void AddPrefab<TEntity>(string name,string prefabPath) where TEntity : class, TEntityInterface =>AddPrefab(new TypeNamePair(typeof(TEntity), name), prefabPath);
         private void AddPrefab(TypeNamePair typeNamePair, string prefabPath)
