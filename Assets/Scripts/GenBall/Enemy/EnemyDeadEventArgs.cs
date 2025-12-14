1ad6d9697bbe9b3b2831885f294e784c4ad7c836
Yueyn <1689759238@qq.com>
2025-12-14 20:49:44 +0800

    怪物死亡事件

---
+++ Assets/Scripts/GenBall/Enemy/EnemyDeadEventArgs.cs
@@ -0,0 +1,23 @@
+using Yueyn.Base.ReferencePool;
+using Yueyn.Event;
+
+namespace GenBall.Enemy
+{
+    public class EnemyDeadEventArgs : GameEventArgs
+    {
+        public override int Id => Index;
+        public static int Index=> typeof(EnemyDeadEventArgs).GetHashCode();
+        public IEnemy Enemy;
+
+        public static EnemyDeadEventArgs Create(IEnemy enemy)
+        {
+            var args = ReferencePool.Acquire<EnemyDeadEventArgs>();
+            args.Enemy = enemy;
+            return args;
+        }
+        public override void Clear()
+        {
+            Enemy=null;
+        }
+    }
+}
