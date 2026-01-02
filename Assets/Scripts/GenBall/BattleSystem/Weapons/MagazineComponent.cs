b3550a86f41ca9bf67fd9bf8a1f86380b6f9866b
Yueyn <1689759238@qq.com>
2026-01-02 23:10:27 +0800

    弹匣组件

---
+++ Assets/Scripts/GenBall/BattleSystem/Weapons/MagazineComponent.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace GenBall.BattleSystem.Weapons
+{
+    public class MagazineComponent : WeaponComponentBase
+    {
+        [SerializeField] private int baseCapacity;
+        public readonly IntStat Capacity = new();
+        public int AmmunitionCount { get;private set; }
+
+        public void Reload()
+        {
+            AmmunitionCount = Capacity.CurrentValue;
+        }
+
+        public bool CanFire(int amount = 1) =>AmmunitionCount>=amount;
+        public void Fire(int amount=1)
+        {
+            if(CanFire()) AmmunitionCount -= amount;
+        }
+
+        protected override void OnEquip()
+        {
+            Capacity.SetBaseValue(baseCapacity);
+            Reload();
+        }
+    }
+}
